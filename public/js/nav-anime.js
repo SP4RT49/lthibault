ea48f039a895c0fe43f7a413e8630addeceeb1b8
unknown <thibault.playstore@gmail.com>
2020-09-22 14:38:58 +0200

    NavBar mid page add

--- public/js/nav-anime.js
+++ public/js/nav-anime.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function(){
     var nav = document.querySelector('header');
-    nav.classList.toggle('sticky', window.scrollY > 940);
+    nav.classList.toggle('sticky', window.scrollY > 860);
     nav.classList.toggle('sticky2', window.scrollY > 1080);
 });
