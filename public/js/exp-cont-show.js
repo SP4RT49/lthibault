1adcd402bfd500dc80fa89e20b313f9a9aaf2603
unknown <thibault.playstore@gmail.com>
2020-09-23 23:35:28 +0200

    Ajout section experience

---
+++ public/js/exp-cont-show.js
@@ -0,0 +1,38 @@
+function text1(){
+
+    var n = "<h1>Titre 1</h1>";
+
+    var d = "<p>description du turfu tahu</p>";
+
+
+    document.getElementById("nom").innerHTML = n;
+    document.getElementById("descri").innerHTML = d;
+}
+
+
+function text2(){
+
+    var n2 = "<h1>Titre 2</h1>";
+    
+    var d2 = "<p>description du turfu tahu2</p>";
+    
+    
+    document.getElementById("nom").innerHTML = n2;
+    document.getElementById("descri").innerHTML = d2;
+}
+
+
+function text3(){
+
+    var n3 = "<h1>Titre 3</h1>";
+        
+     var d3 = "<p>Je suis un passionné de jeux vidéos depuis tout petit. C'est en regardant jouer ma mère sur la Playstation 1 que cette passion m'est venue.</p>"
+     +"<p>J'ai donc commencé à jouer dès mon plus jeune âge sur la Playstation 1 sur des jeux comme Grand Turismo ou Crash bandicoot.</p>"
+     +"<p>Vinrent les époques de la PS3 ou j'ai commencé mes premiers Call of Duty dont j'ai fais la collection et ou j'ai même participé à quelques compétitions.</p>"
+     +"<p> Actuellement je joue sur PC uniquement, sur divers jeux tendance style MMORPG / FPS ou comme des jeux en coopération ou de survie.</p>";
+
+        
+        
+    document.getElementById("nom").innerHTML = n3;
+    document.getElementById("descri").innerHTML = d3;
+}
