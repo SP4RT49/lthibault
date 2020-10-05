96394a825c298e1c0a4104abaa57978910f64d25
unknown <thibault.playstore@gmail.com>
2020-10-05 16:45:23 +0200

    Ajout Page Hub

---
+++ public/js/vue.js
@@ -0,0 +1,51 @@
+Vue.component("modal", {
+  template: "#PortfolioV2",
+});
+
+const PortfolioV2 = {
+  template: "#PortfolioV2",
+  name: "PortfolioV2",
+  data() {
+    return {
+      seen: false,
+    };
+  },
+};
+
+const Portfolio = {
+  template: "#Portfolio",
+  name: "Portfolio",
+  data() {
+    return {
+      seen: false,
+    };
+  },
+};
+
+const Rpg = {
+  template: "#Rpg",
+  name: "Rpg",
+  data() {
+    return {
+      seen: false,
+    };
+  },
+};
+
+// router
+const router = new VueRouter({
+  routes: [
+    {
+      path: "/portfolio-v2",
+      component: PortfolioV2,
+      name: "PortfolioV2",
+      seen: "",
+    },
+    { path: "/portfolio", component: Portfolio, name: "Portfolio" },
+    { path: "/rpg", component: Rpg, name: "Rpg" },
+  ],
+});
+
+const vue = new Vue({
+  router,
+}).$mount("#hub");
